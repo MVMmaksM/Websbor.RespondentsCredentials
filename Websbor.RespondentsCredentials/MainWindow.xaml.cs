0578f7e033d18371f079329b10081f4df335ac84
Maks <Maks@DESKTOP-6KU2K6N>
2023-10-17 00:18:08 +0500

    Создание обработчиков событий

--- Websbor.RespondentsCredentials/MainWindow.xaml.cs
+++ Websbor.RespondentsCredentials/MainWindow.xaml.cs
@@ -37,5 +37,90 @@ namespace Websbor.RespondentsCredentials
             var s = new WebsborContext(db.Options);
             s.Database.EnsureCreated();
         }
+
+        private void MenuItemLoadCredential_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemLoadCatalog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemSaveAllRowCredential_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemSaveCurrentRowsCredential_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemSaveAllRowCatalog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemSaveCurrentRowsCatalog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemShemaLoadCredential_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemShemaLoadCatalog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemCreateDb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemExecuteSqlQuery_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDeleteCredential_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDeleteCatatlog(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemOpenLoadProtocol(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDirectoryProtocol(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemCurrentLogFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDirectoryLogs(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemDeleteLogs(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
